6ddab1710868e4d55a75ad2c45a7a60bb87e6505
NtdChnn <69079640+NtdChnn@users.noreply.github.com>
2020-08-03 20:38:16 +0700

    test

--- TriangleStarsPattern02.cpp
+++ TriangleStarsPattern02.cpp
@@ -2,12 +2,12 @@
 int main()
 {
 	int x,i,j;
-	scanf("%d",&x);
+	scanf("%d",&x); //enter number of layers
 	for(j=1;j<=x;j++)
 	{
 		for(i=1;i<=j;i++)
 		{
-			printf("* ");
+			printf("* "); //print asterisk per layers to make triangle
 		}
 		printf("\n");
 	}
